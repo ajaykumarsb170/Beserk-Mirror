7fee0866d5098482e0b39d0c0e8a94673c602a47
Ajay Ajay <ajaykumarsb170@gmail.com>
2024-12-01 13:57:43 +0000

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -253,7 +253,6 @@ async def get_readable_message(
         else:
             msg += f"<b>{index + start_position}.{tstatus}: </b>"
         msg += f"<code>{escape(f'{task.name()}')}</code>"
-        if tstatus not in 
         
         if tstatus not in [
             MirrorStatus.STATUS_SEEDING,
