2aa6942dac619aecd26008aea3200e7aaf92e41b
Ajay Ajay <ajaykumarsb170@gmail.com>
2024-12-01 12:54:20 +0000

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -184,17 +184,12 @@ def speed_string_to_bytes(size_text: str):
 
 
 def get_progress_bar_string(pct):
-    if isinstance(pct, str):
-        pct = float(pct.strip("%"))
-    p = min(
-        max(pct, 0),
-        100
-    )
-    cFull = int(p // 10)
-    p_str = "█" * cFull
-    p_str += "▒" * (10 - cFull)
-    return f"{p_str}"
-
+    pct = float(pct.strip("%"))
+    p = min(max(pct, 0), 100)
+    cFull = int(p // 8)
+    p_str = "■" * cFull
+    p_str += "□" * (12 - cFull)
+    return f"[{p_str}]"
 
 async def get_readable_message(
         sid,
